814a4efa75437b48c7651e3737853ce79f5965e1
Sahar <100313500+sahardeldar@users.noreply.github.com>
2022-04-06 23:13:46 +0430

    Add files via upload

---
+++ palam_plom.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string computer_choice1, user_choice, computer_choice2;
+            Random r = new Random();
+            int scoruser = 0, scorcomputer1 = 0, scorcomputer2 = 0, index;
+
+            string[] options = new string[2];
+            options[0] = "palam";
+            options[1] = "polom";
+            for (int i = 0; i < 5; i++)
+            {
+                index = r.Next(0, 2);
+                computer_choice1 = options[index];
+                computer_choice2 = options[index];
+                user_choice = Console.ReadLine();
+                Console.WriteLine(computer_choice1);
+                Console.WriteLine(computer_choice2);
+                if (computer_choice1 == "palam " && computer_choice2 == "palam" && user_choice == "polom")
+                {
+                    scoruser++;
+                }
+                if (computer_choice1 == "polom " && computer_choice2 == "palam" && user_choice == "palam")
+                {
+                    scorcomputer1++;
+                }
+                if (computer_choice1 == "palam " && computer_choice2 == "polom" && user_choice == "palam")
+                {
+                    scorcomputer2++;
+                }
+                if (computer_choice1 == "polom " && computer_choice2 == "polom" && user_choice == "palam")
+                {
+                    scoruser++;
+                }
+                if (computer_choice1 == "palam " && computer_choice2 == "polom" && user_choice == "polom")
+                {
+                    scorcomputer1++;
+                }
+                if (computer_choice1 == "polom " && computer_choice2 == "palam" && user_choice == "polom")
+                {
+                    scorcomputer2++;
+                }
+                
+            }
+            if (scorcomputer1>scorcomputer2 && scorcomputer1> scoruser)
+            {
+                Console.WriteLine("computer_choice1 win");
+            }
+            if (scoruser > scorcomputer1 && scoruser > scorcomputer1)
+            {
+                Console.WriteLine("user_choice win");
+            }
+            if (scoruser > scorcomputer1 && scoruser > scorcomputer2)
+            {
+                Console.WriteLine("user_choice win");
+            }
+            if (scorcomputer2 > scorcomputer1 && scorcomputer2 > scoruser)
+            {
+                Console.WriteLine("computer_choice2 win");
+            }
+
+        }
+        }
+}
---
+++ sang_kaghaz.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comp_choi, user_choice;
+            Random r = new Random();
+            int scoruser = 0, scorcom = 0, index;
+
+            string[] options = new string[3];
+            options[0] = "rock";
+            options[1] = "paoer";
+            options[2] = "scissor";
+            for (int i = 0; i < 5; i++)
+            {
+               index = r.Next(0, 3);
+                comp_choi = options[index];
+                user_choice = Console.ReadLine();
+                Console.WriteLine(comp_choi);
+
+                if (comp_choi == "paper " && user_choice == "rock")
+                {
+                    scorcom++;
+                }
+
+                
+                 else if (user_choice == "paper " &&  comp_choi== "rock")
+                {
+                    scoruser++;
+                }
+
+                else if (comp_choi == "scissor " && user_choice == "rock")
+               {
+                    scoruser++;
+                }
+
+                else if (user_choice == "scissor " && comp_choi == "rock")
+                {
+                    scorcom++;
+                }
+                else if (user_choice == "scissor " && comp_choi == "paper")
+                {
+                    scoruser++;
+                }
+                else if (comp_choi == "scissor " && user_choice == "paper")
+                {
+                    scorcom++;
+                }
+                else if (comp_choi == "scissor " && user_choice == "scissor")
+                {
+                    scorcom++;
+                    scoruser++;
+                }
+                else if (comp_choi == "paper " && user_choice == "paper")
+                {
+                    scorcom++;
+                    scoruser++;
+                }
+                else if (comp_choi == "rock " && user_choice == "rock")
+                {
+                    scorcom++;
+                    scoruser++;
+                }
+              
+
+
+            }
+            if(scoruser>scorcom)
+            {
+                Console.WriteLine("you win");
+            }
+            else if(scoruser < scorcom)
+            {
+                Console.WriteLine("computer win");
+            }
+            else
+            {
+                Console.WriteLine("=");
+
+            }
+        }
+    }
+}
